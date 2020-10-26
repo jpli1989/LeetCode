c8dee6d6a28b2296a19317b6bd2e17b01694ce0d
jpli1989 <junpeng.li_1110@yahoo.com>
2020-10-26 16:01:45 +0800

    Update 20ValidParentheses.java

--- 20ValidParentheses.java
+++ 20ValidParentheses.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(c == '(') stack.push(')'); // e.g. if '(' -> stack is ')'
+            else if( c== '[') stack.push(']'); // e.g. if '[' -> stack is ']' 
+            else if(c== '{') stack.push('}'); // e.g. if '{' -> stack is '}'
+            else{
+                if(stack.isEmpty() || c != stack.pop()){ //e.g. if ')' -> stack pop is ')'
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
