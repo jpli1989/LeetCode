1cf200c6e918064dc81367c53e62a326bfa538bf
jpli1989 <junpeng.li_1110@yahoo.com>
2020-10-29 10:44:37 +0800

    Create [array]88MergedSortedArray.java

---
+++ [array]88MergedSortedArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {      
+        int tail1 = m -1, tail2 = n-1;
+        int cur_index = m + n -1;
+        
+        while(tail1 >=0 && tail2 >=0){
+            nums1[cur_index--] = nums1[tail1] > nums2[tail2] ?
+                                nums1[tail1--] : nums2[tail2--];
+        }
+        
+        while(tail2 >=0){
+            nums1[cur_index--] = nums2[tail2--];
+        }
+        
+    }
+}
