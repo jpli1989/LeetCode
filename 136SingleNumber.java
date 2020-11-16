2b39ffaa7b4aeff9cf321cca3d3554b261fdc542
jpli1989 <junpeng.li_1110@yahoo.com>
2020-11-16 09:00:05 +0800

    Create 136SingleNumber.java

---
+++ 136SingleNumber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        
+        for(int num : nums){
+            ans ^= num;
+        }
+        
+        return ans;
+    }
+}
